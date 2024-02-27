07b96bff4d6a1a372b5f3141843b7557db368375
Jason Falk <jasonfalk9@gmail.com>
2024-02-27 15:45:23 -0800

    Fix error for finding database.

--- BestRestaurants/Controllers/CuisinesController.cs
+++ BestRestaurants/Controllers/CuisinesController.cs
@@ -6,18 +6,18 @@ using System.Linq;
 
 namespace BestRestaurants.Controllers
 {
-  public class CuisineController : Controller
+  public class CuisinesController : Controller
   {
     private readonly BestRestaurantsContext _db;
 
-    public CuisineController(BestRestaurantsContext db)
+    public CuisinesController(BestRestaurantsContext db)
     {
       _db = db;
     }
 
     public ActionResult Index()
     {
-      List<Restaurant> model = _db.Restaurants.ToList();
+      List<Cuisine> model = _db.Cuisines.ToList();
       return View(model);
     }
 
