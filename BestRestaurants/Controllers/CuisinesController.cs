86a0f90fb0419f05f0febe54e0665584aa26a7d6
Jason Falk <jasonfalk9@gmail.com>
2024-02-27 13:51:37 -0800

    Add Index route to Cuisine controller.

---
+++ BestRestaurants/Controllers/CuisinesController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestRestaurants.Controllers
+{
+  public class CuisineController : Controller
+  {
+    private readonly ClassNameContext _db;
+
+    public CuisineController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.ToList();
+      return View(model);
+    }
+  }
+}
