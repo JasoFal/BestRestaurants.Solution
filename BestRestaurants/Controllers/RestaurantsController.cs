e4e51103b59b453ade0a3bf0e323eddb578e1522
Jason Falk <jasonfalk9@gmail.com>
2024-02-27 15:47:26 -0800

    Add route for index page.

---
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestRestaurants.Controllers
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.ToList();
+      return View(model);
+    }
+  }
+}
