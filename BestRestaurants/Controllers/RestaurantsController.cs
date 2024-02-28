e2dc46d97c39a41e11e54d776767164363433d4a
Jason Falk <jasonfalk9@gmail.com>
2024-02-28 15:20:35 -0800

    Update edit route to allow ViewBag.

--- BestRestaurants/Controllers/RestaurantsController.cs
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -51,6 +51,14 @@ namespace BestRestaurants.Controllers
       return View(thisRestaurant);
     }
 
+    public ActionResult Edit(int id)
+    {
+      Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(r => r.RestaurantId == id);
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Type"); 
+      return View(thisRestaurant);
+    }
+
+    [HttpPost]
     public ActionResult Edit(Restaurant restaurant)
     {
       _db.Restaurants.Update(restaurant);
